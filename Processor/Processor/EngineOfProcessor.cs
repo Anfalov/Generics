2e6ea704ea54387122bb7150eac2afe571a98e3e
Евгений Анфалов <anfalovd10@yandex.ru>
2016-10-18 05:00:27 +0500

    Processors

---
+++ Processor/Processor/EngineOfProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Processor
+{
+    public class EngineOfProcessor<TEngine>
+    {
+        public EntityOfProcessor<TEngine,TEntity> For<TEntity>()
+        {
+            return new EntityOfProcessor<TEngine, TEntity>();
+        }
+    }
+}
---
+++ Processor/Processor/ProcessorBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Processor
+{
+    public class ProcessorBuilder
+    {
+        public static EngineOfProcessor<TEngine> CreateEngine<TEngine>()
+        {
+            return new EngineOfProcessor<TEngine>();
+        }
+    }
+}
